2b1747377b88573025887378fd159b01c28cc190
Borna Šoštarić <sostaric.borna.dev@hotmail.com>
2026-06-23 01:27:43 +0200

    chore: configure vite src alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   plugins: [
     tailwindcss(),
     tanstackRouter({
