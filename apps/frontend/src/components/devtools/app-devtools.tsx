db140bdb26a7e18c9fb5cd8d3a57833cb6290d9c
Borna Šoštarić <sostaric.borna.dev@hotmail.com>
2026-07-01 14:51:12 +0200

    feat(app): add query devtools

--- apps/frontend/src/components/devtools/app-devtools.tsx
+++ apps/frontend/src/components/devtools/app-devtools.tsx
@@ -1,11 +1,17 @@
 import { TanStackDevtools } from "@tanstack/react-devtools";
 import { formDevtoolsPlugin } from "@tanstack/react-form-devtools";
+import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
 import { LocaleDevtoolsPanel } from "@/components/devtools/locale-devtools";
 
 const devtoolsPlugins = [
   formDevtoolsPlugin(),
+  {
+    id: "tanstack-query",
+    name: "TanStack Query",
+    render: <ReactQueryDevtoolsPanel />,
+  },
   {
     id: "tanstack-router",
     name: "TanStack Router",
