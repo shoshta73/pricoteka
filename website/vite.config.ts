f2ef3ff3eb55a1013d61808020bb8854043f984a
Borna Šoštarić <sostaric.borna.dev@hotmail.com>
2026-07-07 08:53:15 +0200

    chore(website): inline vite config

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,1 +1,28 @@
-export { default } from "../vite.website.config";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
+import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  plugins: [
+    tailwindcss(),
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    visualizer(),
+    visualizer({
+      filename: "stats-sunburst.html",
+      template: "sunburst",
+    }),
+    visualizer({
+      filename: "stats.json",
+      template: "raw-data",
+    }),
+  ],
+});
